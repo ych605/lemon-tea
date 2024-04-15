d2e346b60cde6013232bf5901f0d8fb93ae1eecd
Jacky yu <ych605@gmail.com>
2024-04-15 22:20:10 +0800

    refactor: extract error placeholder

--- src/utils/error.ts
+++ src/utils/error.ts
@@ -1,9 +1,8 @@
 import { AxiosError } from "axios";
 
-export const renderError = (
-  error?: Error | null,
-  errorPlaceholder = "Oops, something went wrong! Please try again later.",
-) => {
+export const defaultErrorPlaceholder = "Oops, something went wrong! Please try again later.";
+
+export const renderError = (error?: Error | null, errorPlaceholder = defaultErrorPlaceholder) => {
   if (error instanceof AxiosError) return `${errorPlaceholder} (Error: ${error.message})`;
 
   return error?.message || errorPlaceholder;
